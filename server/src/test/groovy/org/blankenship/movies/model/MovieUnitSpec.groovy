69df968b51b8f73af0d810510a21dfc967c62365
dustin.r.blankenship <dustin.r.blankenship@accenturefederal.com>
2020-05-07 12:56:42 -0500

    removing second unit test because for some reason it's not running. I'll have to look into it more later

--- server/src/test/groovy/org/blankenship/movies/model/MovieUnitSpec.groovy
+++ server/src/test/groovy/org/blankenship/movies/model/MovieUnitSpec.groovy
@@ -19,20 +19,4 @@ class MovieUnitSpec extends Specification {
         and: 'toString() should contain the title'
             assert movie.toString().contains('Spider-Man: Homecoming')
     }
-
-    @Test
-    void 'a basic model setter test'() {
-        given: 'a Movie with the title of Spider-Man: Homecoming'
-        Movie movie = new Movie()
-            movie.setTitle('Spider-Man: Homecoming')
-            movie.setId(1)
-
-        when: 'getting the title'
-            String title = movie.getTitle()
-            Long id = movie.getId()
-
-        then: 'title should be the same'
-            assert title == 'Spider-Man: Homecoming'
-            assert id == 1
-    }
 }
