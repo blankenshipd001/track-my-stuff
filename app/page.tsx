efd53b697693a4f8970b375f5927ae48e992cd04
code-monkey <blankenshipd001@gmail.com>
2023-11-08 10:29:35 -0600

    Got the providers to load

--- app/page.tsx
+++ app/page.tsx
@@ -55,101 +55,54 @@ const MovieSearch = () => {
     const getMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${movie_api_key}&query=${searchValue}&include_adult=false&language=en-US&append_to_response=providers`;
     const getTvUrl = `https://api.themoviedb.org/3/search/tv?api_key=${movie_api_key}&query=${searchValue}&include_adult=false&language=en-US&append_to_response=providers`;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const newItems: any = [];
-
-    Promise.all([fetch(getMovieUrl), fetch(getTvUrl)]).then(
-      async ([movieResponse, tvResponse]) => {
-        const movieResponseJson = await movieResponse.json();
-        const tvResponseJson = await tvResponse.json();
-
-        console.log("movies", movieResponseJson.results);
-        console.log("tv", tvResponseJson.results);
+    Promise.all([fetch(getMovieUrl), fetch(getTvUrl)])
+      .then((results) => Promise.all(results.map((r) => r.json())))
+      .then(async ([movieResponseJson, tvResponseJson]) => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        movieResponseJson.results.map((movie: any) => {
-          const newMovie = {
-            ...movie,
-            // For now we only care about US but we could expand
-            // providers: providers.results.US,
-          };
-          newItems.push(newMovie);
-        });
+        const movies = await Promise.all(
+          movieResponseJson.results.map((movie: any) => {
+            return fetch(
+              `https://api.themoviedb.org/3/movie/${movie.id}/watch/providers?api_key=${movie_api_key}&external_source=imdb_id`
+            )
+              .then((res) => res.json())
+              .then((providers) => {
+                // console.log("providers", providers);
+                const newMovie = {
+                  ...movie,
+                  // For now we only care about US but we could expand
+                  providers: providers.results.US,
+                };
+
+                return newMovie;
+              });
+          })
+        );
+
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        tvResponseJson.results.map((tv: any) => {
-          const newMovie = {
-            ...tv,
-            // For now we only care about US but we could expand
-            // providers: providers.results.US,
-          };
-          newItems.push(newMovie);
-        });
-        console.log("stuff", newItems);
-        console.log("user", auth.currentUser);
-        setMovies(newItems);
-      }
-    );
+        const tv = await Promise.all(
+          tvResponseJson.results.map((tv: any) => {
+            return fetch(
+              `https://api.themoviedb.org/3/tv/${tv.id}/watch/providers?api_key=${movie_api_key}&external_source=imdb_id`
+            )
+              .then((res) => res.json())
+              .then((providers) => {
+                // console.log("providers", providers);
+                const newShow = {
+                  ...tv,
+                  // For now we only care about US but we could expand
+                  providers: providers.results.US,
+                };
+
+                return newShow;
+              });
+          })
+        );
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const everything: any = [...movies, ...tv];
+        return setMovies(everything);
+      });
   };
-  // const findMovieByTitle = async () => {
-  //   const omdbUrl = `https://www.omdbapi.com/?s=${searchValue}&apikey=${omdb_api_key}`;
-  //   const response = await fetch(omdbUrl);
-  //   const responseJson = await response.json();
-
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //   const newItems: any = [];
-
-  //   const results: [] = await responseJson.Search;
-  //   console.log(results);
-
-  //   if (results !== undefined && results.length > 0) {
-  //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     results.map(async (movie: any) => {
-  //       if (movie.imdbID !== null) {
-  //         // we have a movie so get the details and where it's streaming
-  //         const movieDBUrl = `https://api.themoviedb.org/3/find/${movie.imdbID}?api_key=${movie_api_key}&external_source=imdb_id`;
-
-  //         const streamingProviderUrl = `https://api.themoviedb.org/3/movie/${movie.imdbID}/watch/providers?api_key=${movie_api_key}&external_source=imdb_id`;
-  //         const tvStreamingProviderUrl = `https://api.themoviedb.org/3/tv/${movie.imdbID}/watch/providers?api_key=${movie_api_key}&external_source=imdb_id`
-
-  //         const providerUrl = movie.Type === 'movie' ? streamingProviderUrl : tvStreamingProviderUrl;
-
-  //         Promise.all([fetch(movieDBUrl), fetch(providerUrl)])
-  //           .then(async ([movieResp, providersResp]) => {
-  //             const moviesRes = await movieResp.json();
-  //             const providers = await providersResp.json();
-
-  //             console.log('movies', moviesRes);
-  //             console.log(providers);
-
-  //             if (moviesRes.movie_results.length > 0) {
-  //               const newMovie = {
-  //                 ...moviesRes.movie_results[0],
-  //                 ...movie,
-  //                 // For now we only care about US but we could expand
-  //                 providers: providers.results.US,
-  //               };
-  //               newItems.push(newMovie);
-  //             }
-
-  //             if (moviesRes.tv_results.length > 0) {
-  //               const newMovie = {
-  //                 ...moviesRes.tv_results[0],
-  //                 ...movie,
-  //                 // For now we only care about US but we could expand
-  //                 providers: providers.results.US,
-  //               };
-  //               newItems.push(newMovie);
-  //             }
-  //           })
-  //           .catch((err) => {
-  //             console.log("error", err);
-  //           });
-  //       }
-  //     });
-
-  //     console.log("movies", newItems);
-  //     setMovies(newItems);
-  //   }
-  // };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const addToWatchList = async (movie: any) => {
